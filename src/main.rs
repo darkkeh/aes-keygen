08e02b52c96b70d5738c250b1062d5596bf703fc
darkyshiny <darkyshiny@hotmail.com>
2019-04-16 14:13:15 +0200

    Remove generation of IV

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,14 @@ fn main() {
     // Get randomness straight from the operating system
     let mut gen = OsRng::new().ok().expect("Failed to get OS random generator");
 
-    // Create arrays for key and iv
+    // Create array for key
     let mut key = [0; 32];
-    let mut iv = [0; 16];
 
-    // Fill arrays with random values
+    // Fill the array with random values
     gen.fill_bytes(&mut key[..]);
-    gen.fill_bytes(&mut iv[..]);
 
-    // Encode and print the arrays
+    // Encode and print the array
     println!("Key: {}", encode(&key));
-    println!("IV: {}", encode(&iv));
 
     // Wait for user to hit enter before we exit
     let mut stdin = io::stdin();
